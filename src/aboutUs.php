8f11576f0f13b978ad8b56be941b2865e031078d
Thiago Varallo <thiagovaralllo@gmail.com>
2023-12-04 23:44:09 -0300

    add text in aboutUs

--- src/aboutUs.php
+++ src/aboutUs.php
@@ -28,7 +28,29 @@ session_start();
         </div>
 
         <article>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, iusto quos assumenda eveniet ex quod architecto fuga, debitis modi incidunt, dolorum consectetur? Tenetur sequi explicabo dicta hic? Rerum, dolores sit. Optio velit laborum atque cupiditate qui illum nihil delectus ratione architecto eius rem consectetur aut dolore commodi id dolores mollitia asperiores, ea fuga voluptatum! Quo officia possimus, maxime et placeat eos fugit porro. Accusamus, earum vero pariatur eius ratione tempora neque dignissimos possimus dolores consequatur molestiae debitis et architecto magni dolorum? Incidunt quae earum sit consequatur, quisquam neque explicabo commodi nobis recusandae laboriosam, velit est. Quae, exercitationem? Nostrum perspiciatis nesciunt quia adipisci impedit, hic distinctio vitae ullam minus deleniti, ea pariatur doloribus ipsa? Soluta magni cumque, ea quam ex obcaecati voluptatum fugiat quia provident, veniam laborum officiis repellendus id? Animi eveniet culpa quas, quae molestiae explicabo aperiam, magni ad a dolores similique? Pariatur quasi accusamus eius expedita ad sunt itaque tempore aut officiis. Velit libero ipsam ipsum earum ipsa, laborum cupiditate? Architecto delectus exercitationem sequi voluptatibus aspernatur, voluptatum nulla, minus maxime magni atque a voluptatem perspiciatis, aperiam culpa veniam quaerat. Doloribus dolore architecto illum vel iusto, quasi a magnam quaerat nam, et ipsa dicta mollitia ipsum nesciunt quisquam repudiandae eveniet.</p>
+            <p>Bem-vindo ao Santuário dos Viajantes, seu refúgio acolhedor na encantadora cidade de Aparecida, São Paulo. No coração desta cidade repleta de história e fé, o Santuário dos Viajantes se destaca como um oásis de conforto e hospitalidade, projetado para oferecer uma experiência única aos nossos hóspedes.</p>
+            <h3>Nossa História:</h3>
+            <p>Fundado com a visão de ser mais do que um simples local de hospedagem, o Santuário dos Viajantes nasceu da paixão por proporcionar momentos especiais a cada hóspede que cruza nossas portas. Inspirados pela rica cultura e espiritualidade de Aparecida, buscamos criar um ambiente que reflete não apenas a beleza da região, mas também a calorosa hospitalidade que é tão característica deste local.</p>
+
+            <h3>Localização Privilegiada:</h3>
+            <p>Nosso hotel está estrategicamente situado, a poucos passos dos principais pontos turísticos de Aparecida, incluindo o Santuário Nacional de Nossa Senhora Aparecida. Esta proximidade permite que nossos hóspedes mergulhem nas maravilhas locais, explorando as belezas da cidade com facilidade.</p>
+
+            <h3>Acomodações Aconchegantes:</h3>
+            <p>No Santuário dos Viajantes, dedicamos especial atenção ao conforto dos nossos hóspedes. Nossas acomodações foram projetadas para oferecer um ambiente acolhedor, combinando elegância moderna com toques tradicionais. Cada quarto é um santuário privado, proporcionando o descanso necessário após um dia de descobertas e aventuras.</p>
+
+            <h3>Gastronomia que Encanta:</h3>
+            <p>Delicie-se com a culinária irresistível do nosso restaurante, onde chefs talentosos preparam pratos que são um verdadeiro banquete para os sentidos. Seja no café da manhã, almoço ou jantar, cada refeição no Santuário dos Viajantes é uma experiência gastronômica única.</p>
+
+            <h3>Eventos Memoráveis:</h3>
+            <p>Além de ser um destino ideal para viagens individuais e em família, o Santuário dos Viajantes também é perfeito para eventos especiais. Seja um casamento romântico, uma conferência corporativa ou uma reunião íntima, nossa equipe está comprometida em tornar cada evento memorável e sem complicações.</p>
+
+            <h3>Hospitalidade com Alma:</h3>
+            <p>No coração do Santuário dos Viajantes está nossa equipe dedicada e acolhedora. Estamos aqui para tornar sua estadia verdadeiramente especial, proporcionando um atendimento personalizado e atencioso que reflete a essência da nossa hospitalidade.</p>
+
+            <h3>Descubra Aparecida Conosco:</h3>
+            <p>Convidamos você a explorar Aparecida enquanto desfruta do conforto e da elegância do Santuário dos Viajantes. Seja uma jornada espiritual, uma escapada romântica ou uma viagem em família, estamos aqui para tornar sua estadia inesquecível.</p>
+
+            <p>Seja bem-vindo ao Santuário dos Viajantes, onde cada momento é uma celebração da vida, da fé e da beleza que Aparecida tem a oferecer.</p>
         </article>
     </main>
 </body>
